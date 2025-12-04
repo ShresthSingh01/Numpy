fcca9d51a8b578a30eeb6d59e6bee8862c01f06b
SHRESTH SINGH <shresthsingh112233@gmail.com>
2025-12-04 19:15:15 +0530

    Initial commit

---
+++ First.py
@@ -0,0 +1,11 @@
+'''import numpy as np
+mylist=[1, 2, 3, 4, 5]
+mylist=mylist*2
+print("List multiplied by 2:", mylist)'''
+import numpy as np
+'''myarray=np.array([1, 2, 3, 4, 5])
+myarray=myarray*3
+print("Array multiplied by 2:", myarray)'''
+array=np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R'], ['S', 'T', 'U'], ['V', 'W', 'X']]])
+print(array.shape)
+print(array[0,1,2])
